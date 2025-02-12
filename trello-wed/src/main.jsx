e80a98408099910503727839a51c7a2091848a95
phanvanthuan06052004 <phanthuan652004@gmail.com>
2025-02-12 18:15:39 +0700

    add css baseLine

--- trello-wed/src/main.jsx
+++ trello-wed/src/main.jsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
+import CssBaseline from '@mui/material/CssBaseline' //đồng bồ css giữa các browwer
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <CssBaseline />
     <App />
   </StrictMode>
 )
