4bde61695ccbb20e2a32fb91d3700a2577498e66
phanvanthuan06052004 <phanthuan652004@gmail.com>
2025-02-12 22:30:18 +0700

    add light dark mode version 1

--- trello-wed/src/main.jsx
+++ trello-wed/src/main.jsx
@@ -3,13 +3,13 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import CssBaseline from '@mui/material/CssBaseline' //đồng bồ css giữa các browwer
 import theme from './theme'
-import { ThemeProvider } from '@mui/material/styles'
+import { Experimental_CssVarsProvider as CssVarsProvider } from '@mui/material/styles'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ThemeProvider theme={theme}>
+    <CssVarsProvider theme={theme}>
       <CssBaseline />
       <App />
-    </ThemeProvider>
+    </CssVarsProvider>
   </StrictMode>
 )
