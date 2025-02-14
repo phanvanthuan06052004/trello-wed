068b38b70b5ac5066e2caf1fc1c2449a9e5ba968
phanvanthuan06052004 <phanthuan652004@gmail.com>
2025-02-14 15:52:17 +0700

    relative absolute import.

--- trello-wed/vite.config.js
+++ trello-wed/vite.config.js
@@ -3,6 +3,12 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
   //base: './',
+  //config for relative absolute import path
+  resolve: {
+    alias: [
+      { find: '~', replacement: '/src' }
+    ]
+  }
 })
