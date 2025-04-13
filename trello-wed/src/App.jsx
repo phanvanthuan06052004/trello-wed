db96fe5289fbcd63a527332db71197b0849e16ac
phanvanthuan06052004 <phanthuan652004@gmail.com>
2025-04-13 13:34:17 +0700

    bug

--- trello-wed/src/App.jsx
+++ trello-wed/src/App.jsx
@@ -1,12 +1,18 @@
 import Board from './pages/Boards/_id'
-
-
+import { Navigate, Route, Routes } from 'react-router-dom'
+import NotFound from './pages/404/NotFound'
 function App() {
 
   return (
-    <>
-      <Board />
-    </>
+    <Routes>
+      <Route path='/' element={
+        <Navigate to={'/boards/67e23e3ef43f67ae0a1d8964'} replace={true} /> // replace là xóa đi dấu vết chuyển hướng để tánh user back lại
+      } />
+      <Route path="/boards/:boardId" element={<Board />} />
+
+      {/*các trường hợp route không hợp lệ nó sẽ nhảy xuống đây*/}
+      <Route path='*' element={<NotFound />} />
+    </Routes>
   )
 }
 
--- trello-wed/src/main.jsx
+++ trello-wed/src/main.jsx
@@ -5,22 +5,30 @@ import theme from './theme'
 import { Experimental_CssVarsProvider as CssVarsProvider } from '@mui/material/styles'
 import { ToastContainer } from 'react-toastify'
 import { ConfirmProvider } from 'material-ui-confirm'
+
+// Cấu hình redux
 import { store } from './redux/Store'
 import { Provider } from 'react-redux'
+
+// Cấu hình Router
+import { BrowserRouter } from 'react-router-dom'
 createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
-    <CssVarsProvider theme={theme}>
-      <ConfirmProvider defaultOptions={{
-        allowClose: false,
-        dialogProps: { maxWidth: 'xs' },
-        cancellationButtonProps: { color: 'inherit' },
-        confirmationButtonProps: { color: 'secondary', variant: 'outlined'
-        }
-      }}>
-        <CssBaseline />
-        <App />
-        <ToastContainer />
-      </ConfirmProvider>
-    </CssVarsProvider>
-  </Provider>
+  <BrowserRouter basename='/'>
+    <Provider store={store}>
+      <CssVarsProvider theme={theme}>
+        <ConfirmProvider defaultOptions={{
+          allowClose: false,
+          dialogProps: { maxWidth: 'xs' },
+          cancellationButtonProps: { color: 'inherit' },
+          confirmationButtonProps: {
+            color: 'secondary', variant: 'outlined'
+          }
+        }}>
+          <CssBaseline />
+          <App />
+          <ToastContainer />
+        </ConfirmProvider>
+      </CssVarsProvider>
+    </Provider>
+  </BrowserRouter>
 )
